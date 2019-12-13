6a55f7582ae687cff0dada3a6d71cd978d583f48
dtpthao <dtpthao.its@gmail.com>
2019-12-13 19:56:54 +0200

    clean up

--- Paper/option_Bin.h
+++ Paper/option_Bin.h
@@ -10,7 +10,6 @@ void ScalarMul_Bin_L2R(big k, pepoint P, pepoint R);
 
 /* option_Bin2.cpp */
 inline void PreMul_Bin(pepoint P, pepoint Q, pepoint *plist);
-// use this function
 void ShamirMul_Bin(big a, pepoint P, big b, pepoint Q, pepoint R);
 void TestShrMul_Bin(csprng &Rng, pepoint P, big n, std::string &msg);
 
@@ -21,16 +20,5 @@ void test_bin3(csprng &Rng, pepoint P, big n, std::string msg);
 /* option_Bin_n.cpp */
 void ShamirMul_Bin_n(int n, big *k, pepoint *P, pepoint R);
 void test_bin_n(int d, csprng &Rng, pepoint P, big n, std::string msg);
-
-
-/*******************************************************************************/
-/**_________________________The Very First Option_____________________________**/
-/**                                                                           **/
-void ScalarMul_Bin_R2L(big k, pepoint P, pepoint R);
-void PreMul_Bin_old(pepoint P, pepoint Q, pepoint *plist);
-void ShamirMul_Bin_old(big a, pepoint P, big b, pepoint Q, pepoint R);
-/**_____________________End "The very First Option"___________________________**/
-/*******************************************************************************/
-
 
 #endif
